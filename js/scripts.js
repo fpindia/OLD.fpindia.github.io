c70e107b461e91f7e02470a84138e21a554cf0b8
Manas <prometheansacrifice@gmail.com>
2020-02-20 22:08:46 +0530

    Style fixes

--- js/scripts.js
+++
@@ -1,5 +0,0 @@
-window.addEventListener("DOMContentLoaded", function() {
-  document.querySelector("#nav-button").addEventListener("click", function() {
-    document.querySelector("#nav").classList.toggle("hidden");
-  });
-});
